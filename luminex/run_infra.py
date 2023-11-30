784c450d17c90051e601ae36b8a66280b7700cc3
Maria <m.manishamiranda@gmail.com>
2023-11-30 14:35:54 -0700

    Updating run_infra with Docstrings

--- luminex/run_infra.py
+++ luminex/run_infra.py
@@ -3,11 +3,38 @@ import json
 import os
 
 def read_config(file_path='../config/infra_config.json'):
+
+    '''
+    Returns the static parameters to run_infra from the config file.
+
+            Parameters:
+                    file_path (str): The path of the config file
+
+            Returns:
+                    config_data (dict): Represents the data in the config file
+    '''
+
     with open(file_path, 'r') as config_file:
         config_data = json.load(config_file)
     return config_data
 
 def trigger_workflow(organization, repository, workflow_name, event_type, token, inputs=None):
+
+    '''
+    Triggers the github actions to create the AWS infrastructure for Luminex.
+
+            Parameters:
+                    organization (str): The name of the organization which the Repo belongs to
+                    repository (str): The name of the Repo
+                    workflow_name (str): The github action that needs to be triggered to deploy the infra
+                    event_type (str): The type of the event to trigger
+                    token (str): The personal access token need to trigger the github action
+                    inputs (dict): The inputs variables that needs to be passed to the github action
+
+            Returns:
+                    status_code (int): The code that explains the status of the trigger action.
+    '''
+
     url = f'https://api.github.com/repos/{organization}/{repository}/dispatches'
     print(url)
     headers = {
@@ -38,6 +65,22 @@ def trigger_workflow(organization, repository, workflow_name, event_type, token,
     return response.status_code
 
 def run_infra(pat, stack_name, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY,AWS_SESSION_TOKEN ):
+
+    '''
+    Retrieves values from different sources and finally triggers the function to run the github action
+
+            Parameters:
+                    pat (str): Personal Access token to trigger github action.
+                    stack_name (str): Name of the stack that manages Luminex infra resources.
+                    AWS_ACCESS_KEY_ID (str): AWS Temp Credentials: Access Key ID
+                    AWS_SECRET_ACCESS_KEY (str): AWS Temp Credentials: Secret Access Key
+                    AWS_SESSION_TOKEN (str): AWS Temp Credentials: Session Token
+
+            Returns:
+                    Calls the trigger workflow function with required parameters (From config file: organization_name, repository_name
+                    workflow_name, event_type, From user: personal_access_token, workflow_inputs)
+    '''
+
     config = read_config('../config/infra_config.json')
     organization_name = config.get('GITHUB_ORGANIZATION', 'your-organization')
     repository_name = config.get('GITHUB_REPOSITORY', 'your-username/your-repo')
