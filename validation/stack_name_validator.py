b8afc969cb41c4517398662edb74357a6a033397
Prashanth-mamunuru <venkatasaiprashant.m@dish.com>
2023-12-05 12:24:43 -0700

    checks

---
+++ validation/stack_name_validator.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import boto3
+
+def stack_exists(stack_name):
+    cf_client = boto3.client('cloudformation')
+
+    try:
+        cf_client.describe_stacks(StackName=stack_name)
+        return True  # Stack exists
+    except cf_client.exceptions.ClientError as e:
+        if 'does not exist' in str(e):
+            return False  # Stack does not exist
+        else:
+            raise  # Some other error
+
+if __name__ == "__main__":
+    # Check if the stack name is provided as input from GitHub Actions
+    stack_name = os.getenv('input-stack-name')
+
+    if not stack_name:
+        print("Error: input-stack-name not set.")
+        sys.exit(1)
+
+    # AWS CloudFormation Stack Existence Check
+    if stack_exists(stack_name):
+        print(f"Stack '{stack_name}' exists.")
+        sys.exit(0)  # Exit with success status
+    else:
+        print(f"Stack '{stack_name}' does not exist.")
+        sys.exit(1)  # Exit with failure status
+
+
