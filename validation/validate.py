763bd9267585f693f447b0681e561f063eb80b68
geetamakineni <geetanjalimakineni@gmail.com>
2023-12-05 10:09:55 -0700

    Permissions Validator

--- validation/validate.py
+++ validation/validate.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     # Create an instance of the InputValidator class
     input_validator = InputValidator('config.json')
     
-    # Run the validation
+    # Run the Input validation
     input_validator.run_validation()
     
     # Create an instance of the ETLFileValidator class 
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     # Run the ETL logic validation
     etl_validator.validate_logic_file()
 
-    # Create an instance of IAMRoleValidator and run the permissions validator
+    # Create an instance of IAMRoleValidator and run the Permissions validator
     permissions_validator = IAMRoleValidator('config.json')
     permissions_validator.validate_roles()
