906b006d6cf412589729c3f87dc9ab70b4dec399
Sindhu Chirumamilla <sindhu.chirumamilla@gmail.com>
2023-12-06 13:47:52 -0700

    test method name changed

--- tests/test_s3_data_loader.py
+++ tests/test_s3_data_loader.py
@@ -1,6 +1,9 @@
-import pytest
+"""
+Test module for the S3DataLoader class.
+"""
 import os
 import sys
+import pytest
 from pandas import DataFrame
 
 # Add the parent directory to the Python path
@@ -8,37 +11,43 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from data_standardization.s3_data_loader import S3DataLoader
 
 # input variables
-bucket_name = 'luminex'
-csv_key = 'input-data/csv/sales_data_large.csv'
-json_key = 'input-data/json/EmployeeData.json'
-parquet_key = 'input-data/parquet/FlightsData.parquet'
+BUCKET_NAME = 'luminex'
+CSV_KEY = 'input-data/csv/sales_data_large.csv'
+JSON_KEY = 'input-data/json/EmployeeData.json'
+PARQUET_KEY = 'input-data/parquet/FlightsData.parquet'
 
 @pytest.fixture
-def s3_data_loader():
+def test_s3_loader():
+    """
+    Create and return an instance of the S3DataLoader class.
+
+    Returns:
+    - s3_loader (S3DataLoader): An instance of the S3DataLoader class.
+    """
     return S3DataLoader()
 
-def test_create_s3_client(s3_data_loader):
+def test_create_s3_client(test_s3_loader):
     """
     Tests the create_s3_client method, ensuring it returns a valid S3 client.
 
     Expected Outcome:
     - Test passes if create_s3_client returns an S3 client instance.
     """
-    assert s3_data_loader.create_s3_client() is not None
+    assert test_s3_loader.create_s3_client() is not None
 
-def test_read_data_from_s3_supported_file_type(s3_data_loader):
+def test_read_data_from_s3_supported_file_type(test_s3_loader):
     """
-    Tests read_data_from_s3 with an supported file type.
+    Tests read_data_from_s3 with a supported file type.
 
     Expected Outcome:
     - Test passes if the method behaves correctly.
     """
     file_type = 'csv'
-    result = s3_data_loader.read_data_from_s3(bucket_name, csv_key, file_type)
+    result = test_s3_loader.read_data_from_s3(BUCKET_NAME, CSV_KEY, file_type)
 
     assert isinstance(result, DataFrame)
 
-def test_read_data_from_s3_unsupported_file_type(capsys, s3_data_loader):
+def test_read_data_from_s3_unsupported_file_type(capsys, test_s3_loader):
     """
     Tests read_data_from_s3 with an unsupported file type.
 
@@ -46,37 +55,38 @@ def test_read_data_from_s3_unsupported_file_type(capsys, s3_data_loader):
     - Test passes if the method prints the correct error message.
     """
     file_type = 'unsupported_type'
-    result = s3_data_loader.read_data_from_s3(bucket_name, csv_key, file_type)
+    result = test_s3_loader.read_data_from_s3(BUCKET_NAME, CSV_KEY, file_type)
 
     captured = capsys.readouterr()
     expected_error_message = "Unsupported file type. Choose 'csv', 'json', or 'parquet'."
 
-    assert expected_error_message in captured.out, f"Expected: '{expected_error_message}', Actual: '{captured.out}'"
+    error_msg = f"Expected: '{expected_error_message}', Actual: '{captured.out}'"
+    assert expected_error_message in captured.out, error_msg
     assert result is None
 
-def test_read_csv_from_s3(s3_data_loader):
+def test_read_csv_from_s3(test_s3_loader):
     """
     Tests read_csv_from_s3 for reading a CSV file from an S3 bucket.
 
     Expected Outcome:
     - Passes if CSV file is read successfully and DataFrame is returned.
     """
-    assert isinstance(s3_data_loader.read_csv_from_s3(bucket_name, csv_key), DataFrame)
+    assert isinstance(test_s3_loader.read_csv_from_s3(BUCKET_NAME, CSV_KEY), DataFrame)
 
-def test_read_json_from_s3(s3_data_loader):
+def test_read_json_from_s3(test_s3_loader):
     """
     Tests read_json_from_s3 for reading a JSON file from an S3 bucket.
 
     Expected Outcome:
     - Passes if JSON file is read successfully and DataFrame is returned.
     """
-    assert isinstance(s3_data_loader.read_json_from_s3(bucket_name, json_key), DataFrame)
+    assert isinstance(test_s3_loader.read_json_from_s3(BUCKET_NAME, JSON_KEY), DataFrame)
 
-def test_read_parquet_from_s3(s3_data_loader):
+def test_read_parquet_from_s3(test_s3_loader):
     """
     Tests read_parquet_from_s3 for reading a Parquet file from an S3 bucket.
 
     Expected Outcome:
     - Passes if Parquet file is read successfully and DataFrame is returned.
     """
-    assert isinstance(s3_data_loader.read_parquet_from_s3(bucket_name, parquet_key), DataFrame)
+    assert isinstance(test_s3_loader.read_parquet_from_s3(BUCKET_NAME, PARQUET_KEY), DataFrame)
