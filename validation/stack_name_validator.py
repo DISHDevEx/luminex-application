83f14d194fd986c7f32a2d3c919c766081a8b01a
Prashanth-mamunuru <venkatasaiprashant.m@dish.com>
2023-12-06 12:03:42 -0700

    test10

--- validation/stack_name_validator.py
+++ validation/stack_name_validator.py
@@ -1,12 +1,11 @@
-import os
 import sys
 import boto3
 
-def stack_exists(stack_name):
+def stack_exists(input_stack_name):
     cf_client = boto3.client('cloudformation')
 
     try:
-        cf_client.describe_stacks(StackName=stack_name)
+        cf_client.describe_stacks(StackName=input_stack_name)
         return True  # Stack exists
     except cf_client.exceptions.ClientError as e:
         if 'does not exist' in str(e):
@@ -15,19 +14,19 @@ def stack_exists(stack_name):
             raise  # Some other error
 
 if __name__ == "__main__":
-    # Check if the stack name is provided as input from GitHub Actions
-    stack_name = os.getenv('INPUT_STACK_NAME')
-
-    if not stack_name:
-        print("Error: input_stack_name not set.")
+    # Check if the stack name is provided as a command-line argument
+    if len(sys.argv) < 2:
+        print("Error: Stack name not provided.")
         sys.exit(1)
 
+    input_stack_name = sys.argv[1]
+
     # AWS CloudFormation Stack Existence Check
-    if stack_exists(stack_name):
-        print(f"Stack '{stack_name}' exists.")
+    if stack_exists(input_stack_name):
+        print(f"Stack '{input_stack_name}' exists.")
         sys.exit(0)  # Exit with success status
     else:
-        print(f"Stack '{stack_name}' does not exist.")
+        print(f"Stack '{input_stack_name}' does not exist.")
         sys.exit(1)  # Exit with failure status
 
 
