88a9de495819c19f32fef887657d6dff3dc25c95
tamanna.kawatra <tamanna.kawatra@dish.com>
2024-01-08 13:48:24 -0700

    removing unwanted code

--- infrastructure/luminex-bootstrap.sh
+++ infrastructure/luminex-bootstrap.sh
@@ -1,14 +1,3 @@
-# #!/bin/bash
-#
-# #
-# # sudo wget https://search.maven.org/remotecontent?filepath=org/elasticsearch/elasticsearch/8.8.2/elasticsearch-8.8.2.jar -P /usr/lib/spark/jars/
-#
-# #
-# sudo aws s3 cp s3://luminex/jars/elasticsearch-8.8.2.jar /usr/lib/spark/jars/
-# # sudo yum install python-pip
-# sudo pip3 install pyspark elasticsearch seaborn pandas boto3
-
-
 #!/bin/bash
 set -x -e
 
